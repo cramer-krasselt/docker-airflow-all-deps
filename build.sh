5a560b9c8e3fc295a3a077dd84ca5beeb460a028
Mo Omer <momer@c-k.com>
2020-01-21 07:29:28 -0600

    Add k8s boto3 deps to pip

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build --rm --build-arg AIRFLOW_DEPS="all" -t momer/docker-airflow-all-deps .
+docker build --rm \
+	--build-arg AIRFLOW_DEPS="all" \
+	--build-arg PYTHON_DEPS="kubernetes boto3" \
+	-t momer/docker-airflow-all-deps .

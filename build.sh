c5524befb28804ff1e66f50a643518dfac4f3ee1
Mo Omer <momer@c-k.com>
2020-03-04 09:31:51 -0600

    Change to v1.10.9

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker build --rm \
 	--build-arg AIRFLOW_DEPS="all" \
 	--build-arg PYTHON_DEPS="kubernetes boto3" \
-	-t momer/docker-airflow-all-deps .
+	-t momer/docker-airflow-all-deps:1.10.9 .
